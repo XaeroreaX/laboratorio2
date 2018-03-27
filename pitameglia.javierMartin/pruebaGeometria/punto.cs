d8050ef5871537a029527ddd983f328c366baa4e
XaeroreaX <martinpitameglia@gmail.com>
2018-03-27 12:08:51 -0300

    ejercicio 18

---
+++ pitameglia.javierMartin/pruebaGeometria/punto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometria
+{
+    class punto
+    {
+
+        private int x;
+        private int y;
+
+        public void Punto(int X, int Y)
+        {
+            this.SetX(X);
+            this.SetY(Y);
+        }
+
+        // GETTERS
+        public int GetX()
+        {
+            return this.x;
+        }
+
+        public int GetY()
+        {
+            return this.y;
+        }
+
+
+        // SETTERS
+        public void SetX(int n)
+        {
+            this.x = n;
+        }
+
+        public void SetY(int n)
+        {
+            this.y = n;
+        }
+
+
+    }
+}
---
+++ pitameglia.javierMartin/pruebaGeometria/rectangulo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometria
+{
+    class rectangulo
+    {
+        private float area;
+        private float perimetro;
+
+        private punto vertice1;
+        private punto vertice2;
+        private punto vertice3;
+        private punto vertice4;
+
+        public float Area()
+        {
+            return this.area;
+        }
+
+        public float Perimetro()
+        {
+            int largo;
+            int ancho;
+
+           
+
+            largo = this.vertice4.GetX() - this.vertice1.GetX();
+            ancho = this.vertice2.GetY() - this.vertice1.GetY();
+
+            this.perimetro = 2 * (largo + ancho);
+
+            return this.perimetro;
+        }
+
+
+        public rectangulo(punto Vertice1, punto Vertice3)
+        {
+       
+            this.vertice2 = new punto();
+            
+            this.vertice4 = new punto();
+
+            this.vertice1 = Vertice1;
+            this.vertice3 = Vertice3;
+
+
+            this.vertice2.Punto(this.vertice1.GetX(), this.vertice3.GetY());
+
+            this.vertice2.Punto(this.vertice3.GetX(), this.vertice1.GetY());
+
+
+        }
+
+    }
+}
