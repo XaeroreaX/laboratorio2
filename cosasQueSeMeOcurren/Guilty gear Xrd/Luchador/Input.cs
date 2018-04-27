4cbff9927a15abef4e997eefdcbb128bdf992673
XaeroreaX <martinpitameglia@gmail.com>
2018-04-27 17:40:10 -0300

    no tenemos ni los fields de Input

---
+++ cosasQueSeMeOcurren/Guilty gear Xrd/Luchador/Input.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luchador
+{
+    public class Input
+    {
+
+        #region Fields
+
+        private string _entrada;
+
+        private string _name;
+
+        private int _daño;
+
+        private int _cantGolpes;
+
+        private List<string> _conectadoPor;
+
+        private List<string> _conecta;
+
+        private bool air;
+
+
+        #endregion
+
+
+        #region Propeties
+
+
+        #endregion
+
+        #region Methods
+
+        #region Constructor
+
+        #endregion
+
+        #endregion
+
+        #region Operators
+
+
+        #endregion
+
+
+
+    }
+}
---
+++ cosasQueSeMeOcurren/Guilty gear Xrd/Luchador/Luchador.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luchador
+{
+    public abstract class Luchador
+    {
+
+        #region Fields
+
+
+        //ground
+        protected Input _punch;
+
+        protected Input _crounchPucn;
+
+        protected Input _kick;
+
+        protected Input _closeSlash;
+
+        protected Input _fullSlash;
+
+        protected Input _heavySlash;
+
+        protected Input _
+
+
+        #endregion
+
+
+        #region Propeties
+
+
+        #endregion
+
+        #region Methods
+
+        #region Constructor
+
+        #endregion
+
+        #endregion
+
+        #region Operators
+
+
+        #endregion
+
+
+    }
+}
