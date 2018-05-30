2d58485f0e1e423b3f5db6050aad291033059d54
XaeroreaX <martinpitameglia@gmail.com>
2018-05-30 17:22:41 -0300

    Add files via upload

---
+++ pitameglia.javierMartin/CajaDeHerramientasDePity/ClassLibrary1/Storage.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    namespace DepositoGenerico
+    {
+        public class Storage<T>
+        {
+
+            #region Fields
+
+            private int _length;
+
+            private List<T> _list;
+
+            #endregion
+
+
+            #region Methods
+
+            public bool Add(T that)
+            {
+                bool returnAux = false;
+                int i = 0;
+
+
+                if ((object)that == null || this._length <= this._list.Count) return returnAux;
+
+
+                for (i = 0; i < this._list.Count; i++)
+                {
+
+                    if (this._list[i].Equals(that) == true) break;
+
+                }
+
+                if (i == this._list.Count)
+                {
+                    this._list.Add(that);
+                    returnAux = true;
+                }
+
+                return returnAux;
+            }
+
+            private int GetIndex(T that)
+            {
+                int returnAux = 0;
+
+                foreach (T element in this._list)
+                {
+
+                    if (element.Equals(that) == true) break;
+
+
+                    returnAux++;
+                }
+
+                return returnAux;
+
+            }
+
+            public bool remove(T that)
+            {
+                bool returnAux = false;
+                int index = this.GetIndex(that);
+
+                if (index < this._list.Count)
+                {
+
+                    this._list.RemoveAt(index);
+                    returnAux = true;
+                }
+
+                return returnAux;
+            }
+
+            public override string ToString()
+            {
+                string toString = base.ToString() + "\n";
+
+                foreach (T element in this._list)
+                {
+                    toString += element.ToString();
+                }
+
+                return toString;
+            }
+
+            #region Constructor
+
+            public Storage(int length)
+            {
+                this._length = length;
+
+                this._list = new List<T>();
+
+
+            }
+
+            #endregion
+
+            #endregion
+
+        }
+    }
+}
---
+++ pitameglia.javierMartin/CajaDeHerramientasDePity/ClassLibrary1/XML.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+
+namespace ClassLibrary1
+{
+    public class XML<T>
+    {
+
+        public string Save(string path, T Element)
+        {
+
+            string message = "Load is succefull";
+
+            try
+            {
+
+                XmlTextWriter Arch = new XmlTextWriter(path, Encoding.UTF8);
+
+
+                XmlSerializer serializador = new XmlSerializer(typeof(T));
+
+                serializador.Serialize(Arch, Element);
+
+                Arch.Close();
+            }
+
+            catch (Exception miEx)
+            {
+
+                message = miEx.ToString();
+
+            }
+
+            return message;
+
+        }
+
+
+
+        public string Load(string path, out T var)
+        {
+            string message = "Save is succefull";
+
+            
+
+            try
+            {   XmlTextReader Arch = new XmlTextReader(path);
+
+
+                XmlSerializer serializador = new XmlSerializer(typeof(T));
+
+                var = (T)serializador.Deserialize(Arch);
+
+                Arch.Close();
+
+            }
+
+
+            
+
+            catch(Exception miEx)
+            {
+                
+                message = miEx.ToString();
+
+                var = new this();
+
+            }
+
+            return message;
+
+
+        }
+
+        
+    }
+}
