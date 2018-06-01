1311bc3e01d9547a739f0d9321a9d5ad6d12b7bd
XaeroreaX <martinpitameglia@gmail.com>
2018-05-31 23:55:08 -0300

    Add files via upload

--- Javier.Martin.Pitameglia.TP3/Persona/EntidadesAbstractas.cs
+++ Javier.Martin.Pitameglia.TP3/Persona/EntidadesAbstractas.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace EntidadesAbstractas
 {
+
+
+
     public abstract class Persona
     {
 
@@ -28,14 +31,14 @@ namespace EntidadesAbstractas
 
         #region Propeties
 
-        public string Apellido { get { return this._apellido} set { this._apellido = value; } }
+        public string Apellido { get { return this._apellido; } set { this._apellido = value; } }
 
         public int DNI {
             get { return this._dni; }
 
             set {
 
-                this._dni = Persona.ValidarDni(value, this._nacionalidad);
+                this._dni = Persona.ValidarDni(value.ToString(), this._nacionalidad);
                 
             }
 
@@ -155,12 +158,7 @@ namespace EntidadesAbstractas
         #endregion
 
 
-        #region Propeties
-
-
-
-
-        #endregion
+        
 
         #region Methods
 
@@ -181,7 +179,120 @@ namespace EntidadesAbstractas
             return returnAux;
         }
 
+        protected virtual string MostrarDatos()
+        {
+
+            string message = base.ToString(); ;
+
+            message += "\nLEGAJO"+this._legajo+"\n";
+
+            return message;
+
+        }
+
+        protected abstract string ParticiparEnClase();
+
         #region Constructor
+
+
+        public Universitario(int legajo, string nombre, string apellido, string dni, ENacionalidad nacionalidad) : base(nombre: nombre, dni: dni, apellido: apellido, nacionalidad: nacionalidad) { this._legajo = legajo; }
+
+        public Universitario() : this(1, "Unknow", "Unknow", "0", ENacionalidad.Argentino) { }
+
+        #endregion
+
+        #endregion
+
+        #region Operators
+
+        public static bool operator ==(Universitario pg1, Universitario pg2)
+        {
+            bool returnAux = false;
+
+            if((object) pg1 != null && (object) pg2 != null)
+            {
+
+                returnAux = pg1.Equals((object)pg2);
+
+
+            }
+
+
+            return returnAux;
+
+        }
+
+        public static bool operator !=(Universitario pg1, Universitario pg2)
+        {
+            bool returnAux = false;
+
+            if ((object)pg1 != null && (object)pg2 != null)
+            {
+                returnAux = !(pg1 == pg2);
+            }
+
+            return returnAux;
+        }
+
+        #endregion
+
+
+    }
+
+
+
+    public sealed class Alumno : Universitario
+    {
+
+        public enum EEstadoCuenta { AlDia, Deudor, Becado}
+
+        public enum EClase { Programacion, Lavoratorio, Legislacion, SPD}
+
+
+        #region Fields
+
+
+        private EClase _claseQueToma;
+
+        private EEstadoCuenta _estadoCuenta;
+
+
+        #endregion
+
+
+        #region Methods
+
+
+        protected override string MostrarDatos()
+        {
+            string message = "Alumno:\n";
+
+            message += base.MostrarDatos();
+
+            message += "\nESTADO DE CUENTA" + this._estadoCuenta.ToString() + "\n";
+            message += "TOMA CLASE DE" + this._claseQueToma.ToString()+"\n";
+
+            return message;
+        }
+
+        protected override string ParticiparEnClase()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        #region Constructor
+
+        public Alumno(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad, EClase claseQueToma, EEstadoCuenta estadoCuenta) : base(legajo: id, nacionalidad:nacionalidad, apellido: apellido, nombre: nombre, dni: dni)
+        {
+
+            this._claseQueToma = claseQueToma;
+            this._estadoCuenta = estadoCuenta;
+        }
+
+        public Alumno(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad, EClase claseQueToma) : this(nombre: nombre,apellido: apellido, nacionalidad: nacionalidad, dni: dni, id: id, claseQueToma: claseQueToma, estadoCuenta: EEstadoCuenta.AlDia) { }
+
+        public Alumno() : this(id: 1, nombre: "Unknow", apellido: "Unknow", nacionalidad: ENacionalidad.Argentino, claseQueToma: EClase.Programacion, dni:"45") { }
 
         #endregion
 
