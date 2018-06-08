7ac8cf4a9b82020a8090198e4e80700b65ec1a06
XaeroreaX <martinpitameglia@gmail.com>
2018-06-08 16:45:13 -0300

    terminar cascara

---
+++ pitameglia.javierMartin/Javier.Martin.Pitameglia.TP4/Entidades/Correo.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+
+    public enum EEstado { Ingresado, EnViaje, Entregado }
+
+
+    public class Paquete : IMostrar<Paquete>
+    {
+
+
+        #region Propeties
+
+        public string DireccionDeEntrega
+        {
+            get; set;
+
+        }
+
+        public EEstado Estado
+        {
+
+            get; set;
+
+        }
+
+        public string TrackingID { get; set; }
+
+        #endregion
+
+        #region Methods
+
+
+        public string MostrarDatos(IMostrar<Paquete> elemento)
+        {
+
+            Paquete algo =(Paquete) elemento;
+
+            throw new NotImplementedException();
+        }
+
+
+        //public void Mock
+
+
+
+        #region Constructor
+
+        public Paquete(string direccionEntrega, string trackingID)
+        {
+
+            this.DireccionDeEntrega = direccionEntrega;
+            this.TrackingID = trackingID;
+
+
+        }
+
+        
+
+
+
+
+
+        #endregion
+
+        #endregion
+
+        #region Operators
+
+
+        #endregion
+
+
+
+    }
+}
+
+
+
+#region Fields
+
+
+
+#endregion
+
+
+#region Propeties
+
+
+#endregion
+
+#region Methods
+
+#region Constructor
+
+#endregion
+
+#endregion
+
+#region Operators
+
+
+#endregion
