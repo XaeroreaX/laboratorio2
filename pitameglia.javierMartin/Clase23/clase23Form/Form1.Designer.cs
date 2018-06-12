62ed46ee0b1a4b1f1d808fc4898acba7d15ecc18
XaeroreaX <martinpitameglia@gmail.com>
2018-06-12 11:56:32 -0300

    Add files via upload

---
+++ pitameglia.javierMartin/Clase23/clase23Form/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace clase23Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxCantidadARetirar = new System.Windows.Forms.GroupBox();
+            this.txtCantidadARetirar = new System.Windows.Forms.TextBox();
+            this.gbxCantidadDeBilletes = new System.Windows.Forms.GroupBox();
+            this.lblBilleteDeDosPesos = new System.Windows.Forms.Label();
+            this.txtBilletesDeDosPesos = new System.Windows.Forms.TextBox();
+            this.txtBilletesDeCincoPesos = new System.Windows.Forms.TextBox();
+            this.lblBilletesDeCincoPesos = new System.Windows.Forms.Label();
+            this.txtBilletesDeDiezPesos = new System.Windows.Forms.TextBox();
+            this.lblBilletesDeDiezPesos = new System.Windows.Forms.Label();
+            this.txtBilletesDeVeintePesos = new System.Windows.Forms.TextBox();
+            this.lblBilletesDeVeintePesos = new System.Windows.Forms.Label();
+            this.txtBilletesDeCincuentaPesos = new System.Windows.Forms.TextBox();
+            this.lblBilletesDeCincuentaPesos = new System.Windows.Forms.Label();
+            this.txtBilletesDeCienPesos = new System.Windows.Forms.TextBox();
+            this.lblBilletesDeCienPesos = new System.Windows.Forms.Label();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.gbxCantidadARetirar.SuspendLayout();
+            this.gbxCantidadDeBilletes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxCantidadARetirar
+            // 
+            this.gbxCantidadARetirar.Controls.Add(this.txtCantidadARetirar);
+            this.gbxCantidadARetirar.Location = new System.Drawing.Point(30, 25);
+            this.gbxCantidadARetirar.Name = "gbxCantidadARetirar";
+            this.gbxCantidadARetirar.Size = new System.Drawing.Size(281, 42);
+            this.gbxCantidadARetirar.TabIndex = 0;
+            this.gbxCantidadARetirar.TabStop = false;
+            this.gbxCantidadARetirar.Text = "Cantidad a Retirar";
+            // 
+            // txtCantidadARetirar
+            // 
+            this.txtCantidadARetirar.Location = new System.Drawing.Point(6, 16);
+            this.txtCantidadARetirar.Name = "txtCantidadARetirar";
+            this.txtCantidadARetirar.Size = new System.Drawing.Size(269, 20);
+            this.txtCantidadARetirar.TabIndex = 0;
+            // 
+            // gbxCantidadDeBilletes
+            // 
+            this.gbxCantidadDeBilletes.Controls.Add(this.txtBilletesDeCienPesos);
+            this.gbxCantidadDeBilletes.Controls.Add(this.lblBilletesDeCienPesos);
+            this.gbxCantidadDeBilletes.Controls.Add(this.txtBilletesDeCincuentaPesos);
+            this.gbxCantidadDeBilletes.Controls.Add(this.lblBilletesDeCincuentaPesos);
+            this.gbxCantidadDeBilletes.Controls.Add(this.txtBilletesDeVeintePesos);
+            this.gbxCantidadDeBilletes.Controls.Add(this.lblBilletesDeVeintePesos);
+            this.gbxCantidadDeBilletes.Controls.Add(this.txtBilletesDeDiezPesos);
+            this.gbxCantidadDeBilletes.Controls.Add(this.lblBilletesDeDiezPesos);
+            this.gbxCantidadDeBilletes.Controls.Add(this.txtBilletesDeCincoPesos);
+            this.gbxCantidadDeBilletes.Controls.Add(this.lblBilletesDeCincoPesos);
+            this.gbxCantidadDeBilletes.Controls.Add(this.txtBilletesDeDosPesos);
+            this.gbxCantidadDeBilletes.Controls.Add(this.lblBilleteDeDosPesos);
+            this.gbxCantidadDeBilletes.Location = new System.Drawing.Point(30, 73);
+            this.gbxCantidadDeBilletes.Name = "gbxCantidadDeBilletes";
+            this.gbxCantidadDeBilletes.Size = new System.Drawing.Size(281, 211);
+            this.gbxCantidadDeBilletes.TabIndex = 1;
+            this.gbxCantidadDeBilletes.TabStop = false;
+            this.gbxCantidadDeBilletes.Text = "Cantidad de Billetes";
+            // 
+            // lblBilleteDeDosPesos
+            // 
+            this.lblBilleteDeDosPesos.AutoSize = true;
+            this.lblBilleteDeDosPesos.Location = new System.Drawing.Point(11, 39);
+            this.lblBilleteDeDosPesos.Name = "lblBilleteDeDosPesos";
+            this.lblBilleteDeDosPesos.Size = new System.Drawing.Size(65, 13);
+            this.lblBilleteDeDosPesos.TabIndex = 0;
+            this.lblBilleteDeDosPesos.Text = "Billete de 2$";
+            // 
+            // txtBilletesDeDosPesos
+            // 
+            this.txtBilletesDeDosPesos.Location = new System.Drawing.Point(94, 36);
+            this.txtBilletesDeDosPesos.Name = "txtBilletesDeDosPesos";
+            this.txtBilletesDeDosPesos.Size = new System.Drawing.Size(168, 20);
+            this.txtBilletesDeDosPesos.TabIndex = 1;
+            // 
+            // txtBilletesDeCincoPesos
+            // 
+            this.txtBilletesDeCincoPesos.Location = new System.Drawing.Point(94, 62);
+            this.txtBilletesDeCincoPesos.Name = "txtBilletesDeCincoPesos";
+            this.txtBilletesDeCincoPesos.Size = new System.Drawing.Size(168, 20);
+            this.txtBilletesDeCincoPesos.TabIndex = 3;
+            // 
+            // lblBilletesDeCincoPesos
+            // 
+            this.lblBilletesDeCincoPesos.AutoSize = true;
+            this.lblBilletesDeCincoPesos.Location = new System.Drawing.Point(11, 65);
+            this.lblBilletesDeCincoPesos.Name = "lblBilletesDeCincoPesos";
+            this.lblBilletesDeCincoPesos.Size = new System.Drawing.Size(65, 13);
+            this.lblBilletesDeCincoPesos.TabIndex = 2;
+            this.lblBilletesDeCincoPesos.Text = "Billete de 5$";
+            // 
+            // txtBilletesDeDiezPesos
+            // 
+            this.txtBilletesDeDiezPesos.Location = new System.Drawing.Point(94, 88);
+            this.txtBilletesDeDiezPesos.Name = "txtBilletesDeDiezPesos";
+            this.txtBilletesDeDiezPesos.Size = new System.Drawing.Size(168, 20);
+            this.txtBilletesDeDiezPesos.TabIndex = 5;
+            // 
+            // lblBilletesDeDiezPesos
+            // 
+            this.lblBilletesDeDiezPesos.AutoSize = true;
+            this.lblBilletesDeDiezPesos.Location = new System.Drawing.Point(11, 91);
+            this.lblBilletesDeDiezPesos.Name = "lblBilletesDeDiezPesos";
+            this.lblBilletesDeDiezPesos.Size = new System.Drawing.Size(71, 13);
+            this.lblBilletesDeDiezPesos.TabIndex = 4;
+            this.lblBilletesDeDiezPesos.Text = "Billete de 10$";
+            // 
+            // txtBilletesDeVeintePesos
+            // 
+            this.txtBilletesDeVeintePesos.Location = new System.Drawing.Point(94, 114);
+            this.txtBilletesDeVeintePesos.Name = "txtBilletesDeVeintePesos";
+            this.txtBilletesDeVeintePesos.Size = new System.Drawing.Size(168, 20);
+            this.txtBilletesDeVeintePesos.TabIndex = 7;
+            // 
+            // lblBilletesDeVeintePesos
+            // 
+            this.lblBilletesDeVeintePesos.AutoSize = true;
+            this.lblBilletesDeVeintePesos.Location = new System.Drawing.Point(11, 117);
+            this.lblBilletesDeVeintePesos.Name = "lblBilletesDeVeintePesos";
+            this.lblBilletesDeVeintePesos.Size = new System.Drawing.Size(71, 13);
+            this.lblBilletesDeVeintePesos.TabIndex = 6;
+            this.lblBilletesDeVeintePesos.Text = "Billete de 20$";
+            // 
+            // txtBilletesDeCincuentaPesos
+            // 
+            this.txtBilletesDeCincuentaPesos.Location = new System.Drawing.Point(94, 140);
+            this.txtBilletesDeCincuentaPesos.Name = "txtBilletesDeCincuentaPesos";
+            this.txtBilletesDeCincuentaPesos.Size = new System.Drawing.Size(168, 20);
+            this.txtBilletesDeCincuentaPesos.TabIndex = 9;
+            // 
+            // lblBilletesDeCincuentaPesos
+            // 
+            this.lblBilletesDeCincuentaPesos.AutoSize = true;
+            this.lblBilletesDeCincuentaPesos.Location = new System.Drawing.Point(11, 143);
+            this.lblBilletesDeCincuentaPesos.Name = "lblBilletesDeCincuentaPesos";
+            this.lblBilletesDeCincuentaPesos.Size = new System.Drawing.Size(71, 13);
+            this.lblBilletesDeCincuentaPesos.TabIndex = 8;
+            this.lblBilletesDeCincuentaPesos.Text = "Billete de 50$";
+            // 
+            // txtBilletesDeCienPesos
+            // 
+            this.txtBilletesDeCienPesos.Location = new System.Drawing.Point(94, 166);
+            this.txtBilletesDeCienPesos.Name = "txtBilletesDeCienPesos";
+            this.txtBilletesDeCienPesos.Size = new System.Drawing.Size(168, 20);
+            this.txtBilletesDeCienPesos.TabIndex = 11;
+            // 
+            // lblBilletesDeCienPesos
+            // 
+            this.lblBilletesDeCienPesos.AutoSize = true;
+            this.lblBilletesDeCienPesos.Location = new System.Drawing.Point(11, 169);
+            this.lblBilletesDeCienPesos.Name = "lblBilletesDeCienPesos";
+            this.lblBilletesDeCienPesos.Size = new System.Drawing.Size(77, 13);
+            this.lblBilletesDeCienPesos.TabIndex = 10;
+            this.lblBilletesDeCienPesos.Text = "Billete de 100$";
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(360, 63);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 2;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(360, 128);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(360, 239);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 296);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.gbxCantidadDeBilletes);
+            this.Controls.Add(this.gbxCantidadARetirar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbxCantidadARetirar.ResumeLayout(false);
+            this.gbxCantidadARetirar.PerformLayout();
+            this.gbxCantidadDeBilletes.ResumeLayout(false);
+            this.gbxCantidadDeBilletes.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxCantidadARetirar;
+        private System.Windows.Forms.TextBox txtCantidadARetirar;
+        private System.Windows.Forms.GroupBox gbxCantidadDeBilletes;
+        private System.Windows.Forms.TextBox txtBilletesDeCienPesos;
+        private System.Windows.Forms.Label lblBilletesDeCienPesos;
+        private System.Windows.Forms.TextBox txtBilletesDeCincuentaPesos;
+        private System.Windows.Forms.Label lblBilletesDeCincuentaPesos;
+        private System.Windows.Forms.TextBox txtBilletesDeVeintePesos;
+        private System.Windows.Forms.Label lblBilletesDeVeintePesos;
+        private System.Windows.Forms.TextBox txtBilletesDeDiezPesos;
+        private System.Windows.Forms.Label lblBilletesDeDiezPesos;
+        private System.Windows.Forms.TextBox txtBilletesDeCincoPesos;
+        private System.Windows.Forms.Label lblBilletesDeCincoPesos;
+        private System.Windows.Forms.TextBox txtBilletesDeDosPesos;
+        private System.Windows.Forms.Label lblBilleteDeDosPesos;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ pitameglia.javierMartin/Clase23/clase23Form/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace clase23Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.txtBilletesDeCienPesos.Enabled = false;
+            this.txtBilletesDeCincuentaPesos.Enabled = false;
+            this.txtBilletesDeVeintePesos.Enabled = false;
+            this.txtBilletesDeDiezPesos.Enabled = false;
+            this.txtBilletesDeCincoPesos.Enabled = false;
+            this.txtBilletesDeDosPesos.Enabled = false; 
+
+        }
+
+        private void Refrescar()
+        {
+            this.txtBilletesDeCienPesos.Text = "";
+            this.txtBilletesDeCincuentaPesos.Text = "";
+            this.txtBilletesDeVeintePesos.Text = "";
+            this.txtBilletesDeDiezPesos.Text = "";
+            this.txtBilletesDeCincoPesos.Text = "";
+            this.txtBilletesDeDosPesos.Text = "";
+
+        }
+
+
+        private void Calcular(object sender, EventArgs e)
+        {
+
+            Refrescar();
+
+            int cantidad;
+
+            if (int.TryParse(this.txtCantidadARetirar.Text, out cantidad) == true)
+            {
+                if (cantidad >= 100)
+                {
+
+                    int billetesDeCien = cantidad / 100;
+
+                    cantidad = cantidad % 100;
+
+                    this.txtBilletesDeCienPesos.Text = billetesDeCien.ToString();
+
+                }
+
+
+                if (cantidad >= 50)
+                {
+
+                    int billetesDeCincuenta = cantidad / 50;
+
+                    cantidad = cantidad % 50;
+
+                    this.txtBilletesDeCincuentaPesos.Text = billetesDeCincuenta.ToString();
+
+                }
+
+
+                if (cantidad >= 20)
+                {
+
+                    int billetesDeVeinte = cantidad / 20;
+
+                    cantidad = cantidad % 20;
+
+                    this.txtBilletesDeVeintePesos.Text = billetesDeVeinte.ToString();
+
+                }
+
+
+
+                if (cantidad >= 10)
+                {
+
+                    int billetesDeDiez = cantidad / 10;
+
+                    cantidad = cantidad % 10;
+
+                    this.txtBilletesDeDiezPesos.Text = billetesDeDiez.ToString();
+
+                }
+
+
+                if (cantidad >= 5)
+                {
+
+                    int billetesDeCien = cantidad / 5;
+
+                    cantidad = cantidad % 5;
+
+                    this.txtBilletesDeCienPesos.Text = billetesDeCien.ToString();
+
+                }
+
+                if (cantidad >= 2)
+                {
+
+                    int billetesDeDos = cantidad / 2;
+
+                    cantidad = cantidad % 2;
+
+                    this.txtBilletesDeDosPesos.Text = billetesDeDos.ToString();
+
+                }
+
+
+
+                if (cantidad > 0)
+                {
+
+                    MessageBox.Show("sobro " + cantidad);
+
+                }
+            }
+
+
+
+        }
+
+
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+
+            
+
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.btnAceptar.Click += new EventHandler(Calcular);
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Refrescar();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+       
+    }
+}
