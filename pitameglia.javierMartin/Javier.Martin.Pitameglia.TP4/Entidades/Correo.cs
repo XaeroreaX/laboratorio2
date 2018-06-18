b160aea77f30c80c152620f4d48d86b70b278f18
XaeroreaX <martinpitameglia@gmail.com>
2018-06-18 15:17:07 -0300

    Add files via upload

--- pitameglia.javierMartin/Javier.Martin.Pitameglia.TP4/Entidades/Correo.cs
+++ pitameglia.javierMartin/Javier.Martin.Pitameglia.TP4/Entidades/Correo.cs
@@ -3,41 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Data.SqlClient;
+
 
 namespace Entidades
 {
 
+    public delegate void DelegadoEstado(object sender, EventArgs e);
+
     public enum EEstado { Ingresado, EnViaje, Entregado }
-
-
-    public delegate int DelegadoEstado(object sender, EventArgs e);
-        
 
 
     public class Paquete : IMostrar<Paquete>
     {
 
+        #region Fields
+
+        private string _direccionEntrega;
+
+        private EEstado _estado;
+
+        private string _TrackingId;
+
+        
+
+        #endregion
 
         #region Propeties
 
         public string DireccionDeEntrega
         {
-            get; set;
+            get
+            {
+                return this._direccionEntrega;
+            }
+
+            set { this._direccionEntrega = value; }
 
         }
 
         public EEstado Estado
         {
 
-            get; set;
+            get { return this._estado; }
+
+            set { this._estado = value; }
 
         }
 
-        public string TrackingID { get; set; }
+        public string TrackingID
+        {
+            get { return this._TrackingId; }
+            set { this._TrackingId = value; }
+        }
 
         #endregion
 
         #region Methods
+
+        public event DelegadoEstado InformarEstado;
 
 
         public string MostrarDatos(IMostrar<Paquete> elemento)
@@ -45,12 +70,21 @@ namespace Entidades
 
             Paquete algo =(Paquete) elemento;
 
-            return algo.ToString()+"("+this.Estado+")";
+            return String.Format("{0} para {1}", this.TrackingID, this.DireccionDeEntrega);
         }
 
 
         public void MockCicloDeVida()
         {
+            while(this._estado != EEstado.Entregado)
+            {
+                Thread.Sleep(1000);
+
+                if (this._estado == EEstado.Ingresado) this._estado = EEstado.EnViaje;
+
+                if (this._estado == EEstado.EnViaje) this._estado = EEstado.Entregado;
+
+            }
 
 
         }
@@ -58,7 +92,9 @@ namespace Entidades
 
         public override string ToString()
         {
-            return String.Format("{0} para {1}", this.TrackingID, this.DireccionDeEntrega);
+            IMostrar<Paquete> This = this;
+
+            return String.Format("{0} ({1})", this.MostrarDatos(This), this.Estado.ToString());
         }
 
 
@@ -88,14 +124,31 @@ namespace Entidades
 
         public static bool operator ==(Paquete p1, Paquete p2)
         {
+            bool returnAux = false;
 
-            return false;
+            if((object)p1 != null && (object)p2 != null)
+            {
+
+                if (p1._TrackingId == p2._TrackingId) returnAux = true;
+
+            }
+
+            return returnAux;
         }
 
         public static bool operator !=(Paquete p1, Paquete p2)
         {
 
-            return false;
+            bool returnAux = false;
+
+            if ((object)p1 != null && (object)p2 != null)
+            {
+
+                returnAux = !(p1 == p2);
+
+            }
+
+            return returnAux;
         }
 
 
@@ -108,14 +161,22 @@ namespace Entidades
 
     public class Correo : IMostrar<List<Paquete>>
     {
-        
+
+        #region Fields
+
+        private List<Paquete> _paquetes;
+
+        private List<Thread> _mockPaquetes;
+
+        #endregion
 
 
         #region Propeties
 
         public List<Paquete> Paquetes
         {
-            get; set;
+            get { return this._paquetes; }
+            set { this._paquetes = value; }
 
         }
 
@@ -128,7 +189,19 @@ namespace Entidades
 
         public string MostrarDatos(IMostrar<List<Paquete>> elemento)
         {
-            throw new NotImplementedException();
+            string MD = "";
+
+            
+            List<Paquete> elements = (List<Paquete>)elemento;
+            
+            foreach(Paquete element in elements)
+            {
+                MD += element.ToString();
+
+            }
+
+
+            return MD;
         }
 
 
@@ -143,6 +216,7 @@ namespace Entidades
         public Correo()
         {
             this.Paquetes = new List<Paquete>();
+            this._mockPaquetes = new List<Thread>();
        
         }
 
@@ -154,9 +228,29 @@ namespace Entidades
 
         public static Correo operator +(Correo c, Paquete p)
         {
+            if((object) c != null && (object) p != null)
+            {
 
+                bool flag = false;
 
-            return null;
+                for(int i = 0; i < c._paquetes.Count; i++)
+                {
+                    if(p == c._paquetes[i])
+                    {
+                        flag = true;
+                        c._paquetes[i] = (Paquete)p;
+                        break;
+
+                    }
+
+                    if(flag == false)
+                    {
+                        c._paquetes.Add(p);
+                    }
+                }
+            }
+
+            return c;
         }
 
         #endregion
@@ -166,9 +260,15 @@ namespace Entidades
 
     public class PaqueteDAO
     {
+        private SqlCommand _comando;
+
+        private string _conexion;
+
 
         public bool Insertar(Paquete p)
         {
+
+
 
             return false;
         }
