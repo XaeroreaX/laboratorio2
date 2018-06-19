ab7b0e47645ca8620f0354e55d49f59f87c0b361
XaeroreaX <martinpitameglia@gmail.com>
2018-06-19 09:44:52 -0300

    Add files via upload

---
+++ pitameglia.javierMartin/clase25/clase25/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades.Alumnos;
+using Entidades.Externa;
+using Entidades.Externa.Sellada;
+
+
+namespace clase25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Persona person = new Persona("tincho", "pity", 24, Entidades.Alumnos.ESexo.Masculino);
+
+            PersonaExternaSellada persona = new PersonaExternaSellada("Elphe", "ninfomana", 25, Entidades.Externa.Sellada.ESexo.Femenino);
+
+            Console.WriteLine(persona.Apellido);
+
+            
+
+            Console.ReadKey();
+
+        }
+    }
+
+
+    public static class Extensora
+    {
+
+
+        public static string ObtenerInfo(this Persona p)
+        {
+            string toString = "";
+
+            toString = p.Nombre + "---" + p.Apellido + "---" + p.Edad + "---" + p.Sexo.ToString();
+
+            return toString;
+        }
+
+        public static string ObtenerInfoPersonaExternaSellada(this PersonaExternaSellada p)
+        {
+            string toString;
+
+            toString = p.Apellido;
+
+
+            return toString;
+        }
+
+
+    }
+
+    
+}
